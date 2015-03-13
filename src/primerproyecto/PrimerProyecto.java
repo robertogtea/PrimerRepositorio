6d8e87e4f60c2633d903388fd14f198189400939
user <user@lx001>
2015-03-13 13:37:30 -0600

    Primera Subida

---
+++ src/primerproyecto/PrimerProyecto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyecto;
+
+/**
+ *
+ * @author user
+ */
+public class PrimerProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Compañeros este es el proyecto, cada uno cree las clases que acordamos en el paquete primerporyecto
+        System.out.println("Proyecto Para Compartir");
+    }
+    
+}
